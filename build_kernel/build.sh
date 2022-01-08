a98c918c746ebc4aa931bb89bd29511753e0ae2c
Wei Fu <fuweid89@gmail.com>
2022-01-08 14:44:19 +0800

    build_kernel: update build script
    * Add comment to mention that required new version of pahole
    * Use nproc to build
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- build_kernel/build.sh
+++ build_kernel/build.sh
@@ -14,13 +14,23 @@ cd ..
 readonly REPO_BASE="$(pwd -P)"
 source ${REPO_BASE}/helper.sh
 
+# FIXME(fuweid):
+#
+# The BTF requires new version(v1.23) of pahole, which has to build with
+#
+# * sed -i 's/DDWARVES_MINOR_VERSION=21/DDWARVES_MINOR_VERSION=22/' CMakeLists.txt
+# * cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -D__LIB=lib ..
+#
+# The LD_LIBRARY_PATH needs to be added with /usr/local/lib.
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/usr/local/lib
 
 travis_fold start build_kernel "Building kernel in ${KERNEL_REPO_ROOT}"
 
 cd "${KERNEL_REPO_ROOT}"
 cp "${REPO_BASE}/build_kernel/config-latest.x86_64" .config
 
+make clean
 make olddefconfig && make prepare
-make -j 4 all
+make -j $(nproc) all
 
 travis_fold end build_kernel
