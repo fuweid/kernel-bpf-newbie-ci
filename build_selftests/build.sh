f064ec2874873219f74cb8dbe66ea66458d59ead
Wei Fu <fuweid89@gmail.com>
2022-01-08 14:45:50 +0800

    build_selftests: update build script
    * Add comment to mention that required new version of pahole
    * Use nproc to build
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- build_selftests/build.sh
+++ build_selftests/build.sh
@@ -15,9 +15,17 @@ readonly LLVM_VER=14
 readonly REPO_BASE="$(pwd -P)"
 readonly WORKSPACE="${REPO_BASE}/.workspace"
 
+# FIXME(fuweid):
+#
+# The BTF requires new version(v1.23) of pahole, which has to build with
+#
+# * sed -i 's/DDWARVES_MINOR_VERSION=21/DDWARVES_MINOR_VERSION=22/' CMakeLists.txt
+# * cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -D__LIB=lib ..
+#
+# The LD_LIBRARY_PATH needs to be added with /usr/local/lib.
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/usr/local/lib
 
 source ${REPO_BASE}/helper.sh
-
 
 travis_fold start build_selftests "Building bpf selftests in ${KERNEL_REPO_ROOT}"
 
@@ -28,6 +36,8 @@ cd "${KERNEL_REPO_ROOT}"
 # need python(3)-docutils for rst2man
 # need libcap-dev
 #
+make -C "tools/testing/selftests/bpf" clean
+
 make \
 	CLANG=clang-${LLVM_VER} \
 	LLC=llc-${LLVM_VER} \
@@ -35,7 +45,7 @@ make \
   VMLINUX_BTF="${KERNEL_REPO_ROOT}/vmlinux" \
   VMLINUX_H= \
   -C "tools/testing/selftests/bpf" \
-  -j 4
+  -j $(nproc)
 
 rm -rf "${WORKSPACE}/selftests"
 
