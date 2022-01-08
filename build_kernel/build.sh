8f1832de9b468971ae5e9523aa5adcc7a3c565ca
Wei Fu <fuweid89@gmail.com>
2022-01-08 10:24:58 +0800

    build_kernel: based on github.com/libbpf/libbpf/.github
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

---
+++ build_kernel/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Required ENV
+#
+# KERNEL_REPO_ROOT: it is path about linux kernel repo.
+#
+# Based on https://github.com/libbpf/libbpf/commit/7e89be4022f639061dbeae9e8d3a42a20836ec05
+
+set -euo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ..
+
+readonly LLVM_VER=14
+readonly REPO_BASE="$(pwd -P)"
+
+source ${REPO_BASE}/helper.sh
+
+
+travis_fold start build_kernel "Building kernel in ${KERNEL_REPO_ROOT}"
+
+cd "${KERNEL_REPO_ROOT}"
+cp "${REPO_BASE}/build_kernel/config-latest.x86_64" .config
+
+make olddefconfig && make prepare
+make -j 4 all
+
+travis_fold end build_kernel
